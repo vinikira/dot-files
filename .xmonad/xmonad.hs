ec758cb60314e2ac6147f7329e162e6c65c68f81
Vinícius Simões <vinicius.simoes95@gmail.com>
2016-09-01 22:55:31 -0300

    i3 config files

--- .xmonad/xmonad.hs
+++
@@ -1,8 +0,0 @@
-import XMonad
-import XMonad.Hooks.DynamicLog
-
-main = xmonad =<< xmobar defaultConfig
-      { terminal     = "xterm"
-      , modMask      = mod4Mask
-      , borderWidth  = 2
-      }
