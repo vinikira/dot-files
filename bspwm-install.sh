5569f2f7f347246ccbdd796477f7983189491fbd
Vinícius Simões <vinicius.simoes95@gmail.com>
2017-12-07 08:32:27 -0200

    add scripts to polybar, dunstrc and bspwm-install script

--- bspwm-install.sh
+++ bspwm-install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo pacman -S bspwm bspwm-scripts bspwm-ruler shxhkd rofi polybar xdo bluez playerctl
+yaourt -S bspwm bspwm-scripts bspwm-ruler shxhkd rofi polybar xdo bluez bluez-tools playerctl feh scrot redshift dunst ttf-font-awesome
