49d0e54a4d5c775a7e4d1d07f2aeb6e5adeb46b2
Vinícius Simões <Vincius Simes>
2017-12-07 18:51:21 -0200

    add i3lock fancy

--- bspwm-install.sh
+++ bspwm-install.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-yaourt -S bspwm bspwm-scripts bspwm-ruler sxhkd rofi polybar xdo bluez bluez-tools playerctl feh scrot redshift dunst ttf-font-awesome ttf-unifont siji-git
+yaourt -S bspwm bspwm-scripts bspwm-ruler sxhkd rofi polybar xdo bluez 
+bluez-tools playerctl feh scrot redshift dunst ttf-font-awesome ttf-unifont 
+siji-git i3lock-fancy
