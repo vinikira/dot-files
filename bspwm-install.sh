80baa67608a7fa895b8f727da40cc4ab204cfd26
Vinícius Simões <Vincius Simes>
2017-12-07 10:50:42 -0200

    fix packages and add font for polybar example

--- bspwm-install.sh
+++ bspwm-install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-yaourt -S bspwm bspwm-scripts bspwm-ruler shxhkd rofi polybar xdo bluez bluez-tools playerctl feh scrot redshift dunst ttf-font-awesome
+yaourt -S bspwm bspwm-scripts bspwm-ruler sxhkd rofi polybar xdo bluez bluez-tools playerctl feh scrot redshift dunst ttf-font-awesome ttf-unifont siji-git
