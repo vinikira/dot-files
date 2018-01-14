97d382e9aab2df1771975951cdbcefba01f0713b
Vinícius Simões <vinicius.simoes95@gmail.com>
2018-01-14 14:41:21 -0200

    improve bspwm-install script

--- bspwm-install.sh
+++ bspwm-install.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
-yaourt -S bspwm bspwm-scripts bspwm-ruler sxhkd rofi polybar xdo bluez bluez-tools playerctl feh scrot redshift dunst ttf-font-awesome i3lock-fancy wireless_tools
+yaourt -S bspwm \
+bspwm-scripts \
+bspwm-ruler \
+sxhkd \
+rofi \
+polybar \
+xdo \
+bluez \
+bluez-tools \
+playerctl \
+feh \
+nitrogen \
+scrot \
+redshift \
+dunst \
+ttf-font-awesome \
+i3lock-fancy \
+wireless_tools \
+neofetch \
+wal-git \
+--noconfirm
+
+# https://github.com/hallgat89/WAL-theme-previewer-for-Nitrogen
