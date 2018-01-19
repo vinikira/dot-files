a48bc2a49b2bcc34972fd44f2f004db499dcb6af
Vinícius Simões <Vincius Simes>
2018-01-19 09:53:33 -0200

    add .local/bin directory

--- bspwm-install.sh
+++ bspwm-install.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 yaourt -S bspwm \
-bspwm-scripts \
-bspwm-ruler \
-sxhkd \
-rofi \
-polybar \
-xdo \
-bluez \
-bluez-tools \
-playerctl \
-feh \
-nitrogen \
-scrot \
-redshift \
-dunst \
-ttf-font-awesome \
-i3lock-fancy \
-wireless_tools \
-neofetch \
-wal-git \
---noconfirm
+       bspwm-scripts \
+       bspwm-ruler \
+       sxhkd \
+       rofi \
+       polybar \
+       xdo \
+       bluez \
+       bluez-tools \
+       playerctl \
+       feh \
+       nitrogen \
+       scrot \
+       redshift \
+       dunst \
+       ttf-font-awesome \
+       i3lock-fancy \
+       wireless_tools \
+       neofetch \
+       wal-git \
+       qt5-styleplugins \
+       --noconfirm
 
 # https://github.com/hallgat89/WAL-theme-previewer-for-Nitrogen
