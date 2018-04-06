1a63ba51d9f93053a5a9a6a74f559bafc118e44b
Vinícius Simões <vinicius.simoes95@gmail.com>
2018-04-06 15:05:51 -0300

    add notification for new emails

--- .config/polybar/mail.py
+++ .config/polybar/mail.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
-
+from subprocess import call
 import imaplib
+
 obj = imaplib.IMAP4_SSL('outlook.office365.com',993)
 obj.login('your-email','your-password')
 obj.select()
 number = len(obj.search(None, 'UnSeen')[1][0].split())
-if number>0:
-    print(number)
-else:
-    print('')
+
+if number > 0:
+    call(['notify-send', 'You have a new e-mail message!'])
+
+print(number)
