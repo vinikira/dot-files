ab2938266f48a1c0a55ac6c611e7a223a04f1169
Vinícius Simões <vinicius.simoes95@gmail.com>
2018-04-24 10:17:03 -0300

    add email counter

--- .config/polybar/mail.py
+++ .config/polybar/mail.py
@@ -3,11 +3,23 @@ from subprocess import call
 import imaplib
 
 obj = imaplib.IMAP4_SSL('outlook.office365.com',993)
-obj.login('your-email','your-password')
+obj.login('email','passs')
 obj.select()
 number = len(obj.search(None, 'UnSeen')[1][0].split())
 
-if number > 0:
+counter_file = open('/tmp/email-counter', 'w+')
+content_counter = counter_file.read()
+
+if len(content_counter) == 0:
+    content_counter = 0
+
+current_count = int(content_counter)
+
+if number > current_count:
     call(['notify-send', 'You have a new e-mail message!'])
 
-print(number)
+counter_file.write(str(number))
+
+counter_file.close()
+
+print(str(number))
