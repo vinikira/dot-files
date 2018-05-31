1a8df40a13bac3730f92b1aa7d65486e493b0170
Vinícius Simões <vinicius.simoes95@gmail.com>
2018-05-31 12:27:30 -0300

    fix mail script to notify once

--- .config/polybar/mail.py
+++ .config/polybar/mail.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python
 from subprocess import call
 import imaplib
+from os import path
 
+file_path = '/tmp/email-counter'
 obj = imaplib.IMAP4_SSL('outlook.office365.com',993)
-obj.login('email','passs')
+obj.login('vinicius.simoes@runsmart.cloud','runsmart@2018')
 obj.select()
 number = len(obj.search(None, 'UnSeen')[1][0].split())
 
-counter_file = open('/tmp/email-counter', 'w+')
-content_counter = counter_file.read()
+if not path.isfile(file_path) :
+    temp_file = open(file_path, 'w')
+    temp_file.write('0')
+    temp_file.close()
 
-if len(content_counter) == 0:
-    content_counter = 0
+temp_file = open(file_path, 'r')
+content_counter = temp_file.read()
+temp_file.close()
 
 current_count = int(content_counter)
 
 if number > current_count:
     call(['notify-send', 'You have a new e-mail message!'])
 
-counter_file.write(str(number))
-
-counter_file.close()
+temp_file = open(file_path, 'w')
+temp_file.write(str(number))
+temp_file.close()
 
 print(str(number))
