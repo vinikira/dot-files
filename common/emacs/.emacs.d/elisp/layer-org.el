032e24c03330f76589c63238d464b46448e10508
Vinícius Simões <vinicius.simoes95@gmail.com>
2021-01-12 14:13:22 -0300

    [add] org present

--- common/emacs/.emacs.d/elisp/layer-org.el
+++ common/emacs/.emacs.d/elisp/layer-org.el
@@ -97,6 +97,27 @@
 (add-hook 'dired-mode-hook 'org-download-enable)
 ;; =============================================================================
 
+;; Org Present
+;; ============================================================================
+(straight-use-package 'org-present)
+
+(autoload 'org-present "org-present" nil t)
+
+(with-eval-after-load "org-present"
+  (add-hook 'org-present-mode-hook
+            (lambda ()
+              (org-present-big)
+              (org-display-inline-images)
+              (org-present-hide-cursor)
+              (org-present-read-only)))
+  (add-hook 'org-present-mode-quit-hook
+            (lambda ()
+              (org-present-small)
+              (org-remove-inline-images)
+              (org-present-show-cursor)
+              (org-present-read-write))))
+;; =============================================================================
+
 ;; Org + Reveal.js
 ;; =============================================================================
 (straight-use-package 'org-re-reveal)
