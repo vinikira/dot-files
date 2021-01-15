1c1556972be576845ce318a6f5067cc521b5418f
Vinícius Simões <vinicius.simoes95@gmail.com>
2021-01-15 18:53:58 -0300

    [mod] bring vs/scratch-buffer key back and fix function references

--- common/emacs/.emacs.d/elisp/base-global-keys.el
+++ common/emacs/.emacs.d/elisp/base-global-keys.el
@@ -5,20 +5,21 @@
 ;; Set keys
 ;; =============================================================================
 (with-eval-after-load 'dired
-  (define-key dired-mode-map (kbd "e") 'dired-create-empty-file)
-  (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file))
+  (define-key dired-mode-map (kbd "e") #'dired-create-empty-file)
+  (define-key dired-mode-map (kbd "RET") #'dired-find-alternate-file))
 
-(global-set-key (kbd "C-x C-b") 'ibuffer)
-(global-set-key (kbd "C-c i") 'vs/indent-buffer)
-(global-set-key (kbd "H-=") 'maximize-window)
-(global-set-key (kbd "H--") 'minimize-window)
-(global-set-key (kbd "H-0") 'balance-windows)
-(global-set-key (kbd "C-x 2") 'vs/split-window-below-and-switch)
-(global-set-key (kbd "C-x 3") 'vs/split-window-right-and-switch)
-(global-set-key (kbd "M-S-<up>") 'vs/move-line-up)
-(global-set-key (kbd "M-S-<down>") 'vs/move-line-down)
-(global-set-key (kbd "H-d") 'vs/duplicate-current-line)
-(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "C-x C-b") #'ibuffer)
+(global-set-key (kbd "C-c i") #'vs/indent-buffer)
+(global-set-key (kbd "H-=") #'maximize-window)
+(global-set-key (kbd "H--") #'minimize-window)
+(global-set-key (kbd "H-0") #'balance-windows)
+(global-set-key (kbd "C-x 2") #'vs/split-window-below-and-switch)
+(global-set-key (kbd "C-x 3") #'vs/split-window-right-and-switch)
+(global-set-key (kbd "M-S-<up>") #'vs/move-line-up)
+(global-set-key (kbd "M-S-<down>") #'vs/move-line-down)
+(global-set-key (kbd "H-d") #'vs/duplicate-current-line)
+(global-set-key (kbd "M-/") #'hippie-expand)
+(global-set-key (kbd "C-c s b") #'vs/scratch-buffer)
 ;; =============================================================================
 
 ;; Unset keys
