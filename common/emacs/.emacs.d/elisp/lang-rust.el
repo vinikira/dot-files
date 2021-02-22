c52d55f3d817f23263f841adf62b382770fc0a73
Vinícius Simões <vinicius.simoes95@gmail.com>
2021-02-22 12:10:21 -0300

    [fix] remove unused commentaries

--- common/emacs/.emacs.d/elisp/lang-rust.el
+++ common/emacs/.emacs.d/elisp/lang-rust.el
@@ -5,8 +5,6 @@
 ;; Rust Mode
 ;; =============================================================================
 (straight-use-package 'rustic)
-;; (customize-set-variable 'rust-format-on-save t)
-;; (customize-set-variable 'company-tooltip-align-annotations t)
 ;; =============================================================================
 
 (provide 'lang-rust)
