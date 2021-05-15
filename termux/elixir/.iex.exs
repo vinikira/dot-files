eec27e84463d8c3237060dd43e72182cdf3afcb2
Vinícius Simões <vinicius.simoes95@gmail.com>
2021-05-15 14:08:58 -0300

    [add] termux workspace

---
+++ termux/elixir/.iex.exs
@@ -0,0 +1,3 @@
+IEx.configure(
+  colors: [enabled: true]
+)
---
+++ termux/emacs/.emacs.d/init.el
@@ -0,0 +1,45 @@
+(require 'package)
+
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")))
+
+(setq package-archive-priorities
+      '(("melpa" . 3)
+        ("gnu" . 2)
+        ("org" . 1)))
+
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+(require 'use-package-ensure)
+(customize-set-variable 'use-package-always-ensure t)
+
+(hl-line-mode 1)
+(icomplete-mode 1)
+(auto-revert-mode 1)
+(load-theme 'misterioso)
+
+(use-package elixir-mode
+  :mode ("\\.ex$" "\\.exs$" "mix.lock"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(elixir-mode use-package))
+ '(use-package-always-ensure t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
