76e9db50a7d9663bfd070c40dcdd231d29efe846
Vinícius Simões <vinicius.simoes95@gmail.com>
2021-05-16 20:30:04 -0300

    [mod] notify only when have new emails.

--- common/mail/.local/bin/checkmail.sh
+++ common/mail/.local/bin/checkmail.sh
@@ -6,9 +6,16 @@ if [ $STATE = 'full' ]
 then
     mbsync -CVa
     notmuch new
-    notify-send -a emacsclient -i emacs \
-                -i /usr/share/icons/Adwaita/64x64/status/mail-unread-symbolic.symbolic.png \
-                "You have $(notmuch count tag:unread) unread emails!"
+
+    COUNT_MAIL=`notmuch count tag:unread`
+
+    if [ $COUNT_MAIL != '0' ]
+    then
+        notify-send -a emacsclient -i emacs \
+                    -i /usr/share/icons/Adwaita/64x64/status/mail-unread-symbolic.symbolic.png \
+                    "You have $COUNT_MAIL unread emails!"
+    fi
+
     exit 0
 fi
 
