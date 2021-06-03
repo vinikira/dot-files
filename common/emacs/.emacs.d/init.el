afed33e7a99763b46dcee63f151742d109885a2f
Vinícius Simões <vinicius.simoes95@gmail.com>
2021-06-03 20:13:05 -0300

    [mod] git depth when using straight

--- common/emacs/.emacs.d/init.el
+++ common/emacs/.emacs.d/init.el
@@ -18,6 +18,7 @@
 ;; =============================================================================
 (defvar bootstrap-version)
 (customize-set-variable 'straight-check-for-modifications '(check-on-save find-when-checking))
+(customize-set-variable 'straight-vc-git-default-clone-depth 1)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
       (bootstrap-version 5))
