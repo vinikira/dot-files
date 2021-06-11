ae76e1f4b76c61a656e006ec202ac7eb093fa6d5
Vinícius Simões <vinicius.simoes95@gmail.com>
2021-06-11 19:38:53 -0300

    [mod] Load Erlang from installation directory

--- common/emacs/.emacs.d/elisp/lang-erlang.el
+++ common/emacs/.emacs.d/elisp/lang-erlang.el
@@ -2,11 +2,20 @@
 ;;; Commentary:
 ;;; Code:
 
-(declare-function straight-use-package "ext:straight")
-
 ;; Erlang Mode
 ;; ========================================================================================
-(straight-use-package 'erlang)
+(defun vs/load-erlang-mode ()
+  "Detect if erlang is installed and load elisp files from erlang directory."
+  (interactive)
+  (let ((erlang-dir (concat
+                     (string-trim (shell-command-to-string "asdf where erlang"))
+                     "/lib/tools-3.5/emacs")))
+    (if (file-directory-p erlang-dir)
+        (progn
+          (add-to-list 'load-path erlang-dir)
+          (require 'erlang)
+          (message "Erlang mode loaded!"))
+      (warn "Erlang isn't installed..."))))
 ;; ========================================================================================
 
 (provide 'lang-erlang)
