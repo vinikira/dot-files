33d7550f91cb5e6a47ad46b1703ab3491e56d2b2
Vinícius Simões <vinicius.simoes95@gmail.com>
2021-08-09 14:51:26 -0300

    [add] lsp grammarly

--- common/emacs/.emacs.d/elisp/layer-writer.el
+++ common/emacs/.emacs.d/elisp/layer-writer.el
@@ -32,6 +32,18 @@
 (global-set-key (kbd "<f6>") #'darkroom-tentative-mode)
 ;; =============================================================================
 
+;; LSP Grammarly
+;; =============================================================================
+(straight-use-package 'lsp-grammarly)
+
+(defun vs/enable-lsp-grammarly ()
+  "Enable lsp-grammarly."
+  (interactive)
+  (declare-function lsp-deferred "ext:lsp")
+  (require 'lsp-grammarly)
+  (lsp-deferred))
+;; =============================================================================
+
 (provide 'layer-writer)
 
 ;;; layer-writer.el ends here
