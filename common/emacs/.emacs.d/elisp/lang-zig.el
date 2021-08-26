b1e7d317a6dd3658d0140426b7b11b350ffa5292
Vinícius Simões <vinicius.simoes95@gmail.com>
2021-08-25 23:03:00 -0300

    [add] Zig mode

---
+++ common/emacs/.emacs.d/elisp/lang-zig.el
@@ -0,0 +1,13 @@
+;;; lang-zig.el --- Zig lang config -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(declare-function straight-use-package "ext:straight")
+
+;; Zig mode
+;; =============================================================================
+(straight-use-package 'zig-mode)
+;; =============================================================================
+
+(provide 'lang-zig)
+;;; lang-zig.el ends here
