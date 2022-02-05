84858b0b4587d869167c43fcdbb5e4843bb5c02f
Vinícius Simões <vinicius.simoes95@gmail.com>
2022-02-05 00:43:19 -0300

    [mod] migrate backups to restic

--- common/bin/.local/bin/vsimoes-backup.sh
+++ common/bin/.local/bin/vsimoes-backup.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-DATE=$(date +%Y-%m-%d-%H%M%S)
 GDRIVEUSER="google-drive://vinicius.simoes95@gmail.com"
-BACKUP_DIR="$GDRIVEUSER/0AHkduRDdYicdUk9PVA/1M_5mB8h9uiqU4alVuor2MbxoQju8AFeo"
+BACKUP_DIR="$GDRIVEUSER/0AHkduRDdYicdUk9PVA/1iFgetgnJU2KBiLE1c_3Z_48q4J558u1K"
 SOURCE="$HOME/Sync"
-TMP_DIR="/tmp/vsimoes-backups"
-BACKUP_FILE_NAME="backup-$DATE.tar.gz"
+RESTIC_REPO="$HOME/Backups"
+BACKUP_FILE="/tmp/vsimoes-backup.tar.gz"
 
-# make backup and move to tmp dir
-mkdir -p $TMP_DIR
-tar -cvzpf $TMP_DIR/$BACKUP_FILE_NAME $SOURCE
+# make backup
+restic -r $RESTIC_REPO --verbose --password-command='pass show restic' backup $SOURCE
+
+# compress repo
+tar -cvzpf $BACKUP_FILE $RESTIC_REPO
 
 # mount Google Drive
 gio mount $GDRIVEUSER
 
 # copy file to backup dir
-gio copy $TMP_DIR/$BACKUP_FILE_NAME $BACKUP_DIR
+gio copy $BACKUP_FILE $BACKUP_DIR
