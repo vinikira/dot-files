fc5fcede8623f8949419b292454dfa87389ae36f
Vinícius Simões <vinicius.simoes95@gmail.com>
2022-02-07 23:42:53 -0300

    [mod] move backup to own layer and now using rclone instead of gio

--- common/backup/.local/bin/vsimoes-backup.sh
+++ common/backup/.local/bin/vsimoes-backup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-GDRIVEUSER="google-drive://vinicius.simoes95@gmail.com"
-BACKUP_DIR="$GDRIVEUSER/0AHkduRDdYicdUk9PVA/1iFgetgnJU2KBiLE1c_3Z_48q4J558u1K"
+RCLONE_REMOTE="gdrive"
+BACKUP_DIR="$RCLONE_REMOTE:Backups"
 SOURCE="$HOME/Sync"
 RESTIC_REPO="$HOME/Backups"
 BACKUP_FILE="/tmp/vsimoes-backup.tar.gz"
@@ -12,8 +12,5 @@ restic -r $RESTIC_REPO --verbose --password-command='pass show restic' backup $S
 # compress repo
 tar -cvzpf $BACKUP_FILE $RESTIC_REPO
 
-# mount Google Drive
-gio mount $GDRIVEUSER
-
 # copy file to backup dir
-gio copy $BACKUP_FILE $BACKUP_DIR
+rclone copy $BACKUP_FILE $BACKUP_DIR
