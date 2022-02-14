061ba342198c05c493ec4e10766eba04f6485244
Vinícius Simões <vinicius.simoes95@gmail.com>
2022-02-13 21:17:18 -0300

    [mod] improve early-init to have a faster startup

--- common/emacs/.emacs.d/early-init.el
+++ common/emacs/.emacs.d/early-init.el
@@ -10,10 +10,27 @@
                     gc-cons-percentage gc-cp
                     file-name-handler-alist fha))))
 
+;; Native compilation settings
+(when (featurep 'native-compile)
+  ;; Silence compiler warnings as they can be pretty disruptive
+  (setq-default native-comp-async-report-warnings-errors nil)
+
+  ;; Make native compilation happens asynchronously
+  (setq native-comp-deferred-compilation t))
+
 ;; Disable garbage collection on startup
 (setq gc-cons-threshold most-positive-fixnum
       gc-cons-percentage 0.8
       file-name-handler-alist nil)
+
+;; Remove some unneeded UI elements (the user can turn back on anything they wish)
+(setq inhibit-startup-message t)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(push '(background-color . "#232635") default-frame-alist)
+(push '(foreground-color . "#FFFFFF") default-frame-alist)
+(push '(mouse-color . "white") default-frame-alist)
 
 ;; Disable package.el
 (setq package-enable-at-startup nil)
