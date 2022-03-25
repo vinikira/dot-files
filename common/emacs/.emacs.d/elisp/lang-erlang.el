9b4de9fafc9a245532679361a9e1aba82b249853
Vinícius Simões <vinicius.simoes95@gmail.com>
2022-03-25 15:38:26 -0300

    [fix] erlang dynamic load

--- common/emacs/.emacs.d/elisp/lang-erlang.el
+++ common/emacs/.emacs.d/elisp/lang-erlang.el
@@ -7,12 +7,16 @@
 (defun vs/load-erlang-mode ()
   "Detect if erlang is installed and load elisp files from erlang directory."
   (interactive)
-  (let ((erlang-dir (concat
-                     (string-trim (shell-command-to-string "asdf where erlang"))
-                     "/lib/tools-3.5/emacs")))
-    (if (file-directory-p erlang-dir)
+  (let* ((erlang-lib-dir (concat
+                          (string-trim (shell-command-to-string "asdf where erlang"))
+                          "/lib"))
+         (tools-dir (seq-find (lambda (dir-name)
+                                (string-match "^tools.?+" dir-name))
+                              (directory-files erlang-lib-dir)))
+         (erlang-emacs-dir (concat erlang-lib-dir "/" tools-dir "/emacs")))
+    (if (file-directory-p erlang-emacs-dir)
         (progn
-          (add-to-list 'load-path erlang-dir)
+          (add-to-list 'load-path erlang-emacs-dir)
           (require 'erlang)
           (message "Erlang mode loaded!"))
       (warn "Erlang isn't installed..."))))
