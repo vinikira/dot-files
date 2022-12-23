8e91ea45eb4df0c4c99cef21224ecf09aa520b49
Vinícius Simões <vinicius.simoes95@gmail.com>
2022-12-23 15:49:14 -0300

    feat(nvim): vimtest and other remaps

--- common/nvim/.config/nvim/lua/vsimoes/remap.lua
+++ common/nvim/.config/nvim/lua/vsimoes/remap.lua
@@ -1,7 +1,10 @@
-local nnoremap = require('vsimoes.keymap').nnoremap 
+local nnoremap = require('vsimoes.keymap').nnoremap
+local inoremap = require('vsimoes.keymap').inoremap
 
-nnoremap('<leader>w', ':w<CR>')  
-nnoremap('<leader>pv', '<cmd>Ex<CR>')  
+nnoremap('<leader>w', ':w<CR>')
+nnoremap('<leader>q', ':q<CR>')
+nnoremap('<leader>pv', '<cmd>Ex<CR>')
+inoremap('jk', '<esc>')
 
 local telescope = require('telescope.builtin')
 
@@ -9,3 +12,9 @@ nnoremap('<leader>ff', telescope.find_files)
 nnoremap('<leader>fg', telescope.live_grep)
 nnoremap('<leader>fb', telescope.buffers)
 nnoremap('<leader>fh', telescope.help_tags)
+
+nnoremap('<leader>tt', ':TestNearest<CR>')
+nnoremap('<leader>tT', ':TestFile<CR>')
+nnoremap('<leader>ta', ':TestSuite<CR>')
+nnoremap('<leader>tl', ':TestLast<CR>')
+nnoremap('<leader>tg', ':TestVisit<CR>')
