9502f4a6fca3dc5ee516a39aa514e7df35fcb436
Vinícius Simões <vinicius.simoes95@gmail.com>
2023-05-08 11:18:42 -0300

    refact(emacs): move elixir functions to elisp file

---
+++ common/emacs/.emacs.d/site-lisp/vs-elixir.el
@@ -0,0 +1,93 @@
+;;; vs-lib.el --- Elixir helpers -*- lexical-binding: t -*-
+
+;; Author: Vinícius Simões
+;; Maintainer: Vinícius Simões
+;; Version: 0.0.1
+;; Package-Requires: (elixir-mode project xwidget)
+;; Homepage: homepage
+;; Keywords: Elixir Emacs
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; commentary
+
+;;; Code:
+
+(require 'project)
+
+;;;###autoload
+(defun vs/json-to-etf (&optional begin end)
+  "Transform JSON to Elixir Term Format.  Use BEGIN and END as region."
+  (interactive "r")
+  (save-excursion
+    (replace-regexp-in-region "\": " "\" => " begin end)
+    (replace-regexp-in-region "{" "%{" begin end)
+    (replace-regexp-in-region "null" "nil" begin end)))
+
+;;;###autoload
+(defun vs/etf-to-json (&optional begin end)
+  "Transform Elixir Term Format to JSON.  Use BEGIN and END as region."
+  (interactive "r")
+  (save-excursion
+    (replace-regexp-in-region "\" => " "\": " begin end)
+    (replace-regexp-in-region "%{" "{" begin end)
+    (replace-regexp-in-region "nil" "null" begin end)))
+
+;;;###autoload
+(defun vs/elixir-map-atom-to-map-string (&optional begin end)
+  "Transform Elixir map atom to map string.  Use BEGIN and END as region."
+  (interactive "r")
+  (save-excursion
+    (replace-regexp-in-region "\\([a-zA-z0-9]+\\): " "\"\\1\" => " begin end)))
+
+;;;###autoload
+(defun vs/elixir-map-string-to-map-atom (&optional begin end)
+  "Transform Elixir map string to map atom.  Use BEGIN and END as region."
+  (interactive "r")
+  (save-excursion
+    (replace-regexp-in-region "\"\\([a-zA-z0-9]+\\)\" =>" "\\1: " begin end)))
+
+;;;###autoload
+(defun vs/elixir-open-dep-docs (&optional force-external)
+  "Open the choosen dep in hexdocs using xwidget-webkit if available.
+FORCE-EXTERNAL browser if provided."
+  (interactive "P")
+  (unless (project-current)
+    (user-error "Not in a project"))
+  (let* ((default-directory (project-root (project-current)))
+         (url
+          (with-temp-buffer
+            (insert (shell-command-to-string "mix deps"))
+            (kill-matching-lines "^  " (point-min) (point-max))
+            (replace-regexp-in-region "^\* \\([a-zA-Z_]+\\) \\([0-9\.]+\\) .+$"
+                                      "https://hexdocs.pm/\\1/\\2"
+                                      (point-min)
+                                      (point-max))
+            (completing-read "select the dependency: "
+                             (split-string (string-trim (buffer-string)) "\n")))))
+    (if force-external
+        (browse-url url)
+      (if (featurep 'xwidget)
+          (xwidget-webkit-browse-url url t)
+        (browse-url url)))))
+
+(provide 'vs-elixir)
+
+;;; vs-elixir.el ends here
