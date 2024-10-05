02cbc647577b0b51e17e848d77a082019ef444e1
Vinícius Simões <vinicius.simoes95@gmail.com>
2024-10-04 21:44:12 -0300

    [nvim] add treesj plugin

---
+++ common/nvim/.config/nvim/lua/vsimoes/lazy/treesj.lua
@@ -0,0 +1,8 @@
+return {
+  'Wansmer/treesj',
+  keys = { '<leader>m' },
+  dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
+  config = function()
+    require('treesj').setup({--[[ your config ]]})
+  end,
+}
