3cbf411dc0ac2d9f61d587b90ce71b3bb1b0592d
Vinícius Simões <vinicius.simoes95@gmail.com>
2025-07-23 13:55:35 -0300

    chore(lem): font config, some modes and hooks

--- common/lem/.config/lem/init.lisp
+++ common/lem/.config/lem/init.lisp
@@ -1,5 +1,17 @@
 (in-package :lem-user)
 
+;; Font
+
+#+lem-sdl2
+(progn
+  (let ((font "/Users/vinicius.simoes/Library/Fonts/IosevkaNerdFont-Regular.ttf")
+        (bold-font "/Users/vinicius.simoes/Library/Fonts/IosevkaNerdFont-Bold.ttf"))
+    (when (and (probe-file font) (probe-file bold-font))
+      (lem-sdl2/display:change-font (lem-sdl2/display:current-display)
+                                    (lem-sdl2/font:make-font-config
+                                     :latin-normal-file font
+                                     :latin-bold-file bold-font
+                                     :size 16)))))
 ;; Completion system
 
 (setf lem-core::*default-prompt-gravity* :bottom-display)
@@ -28,3 +40,38 @@
 
 (define-command open-init-file ()
     (lem:find-file (merge-pathnames "init.lisp" (lem-home))))
+
+;; Modes
+
+(lem-paredit-mode:paredit-mode t)
+(lem/line-numbers::line-numbers-mode t)
+
+;; Trailing spaces before save
+
+(add-hook lem/buffer/file:before-save-hook
+          (lambda ()
+            (delete-trailing-whitespace)))
+
+;; elixir mode lsp
+
+(setf (lem-lsp-mode/spec:spec-command
+       (lem-lsp-mode/spec:get-language-spec 'lem-elixir-mode:elixir-mode))
+      '("sh" "~/.emacs.d/cache/lsp/elixir-ts-mode/lexical/bin/start_lexical.sh"))
+
+;; Meta as CMD on MacOS (hack way)
+
+(in-package :lem-sdl2/keyboard)
+
+(defparameter *modifier-code-table*
+  `((:shift ,sdl2-ffi:+kmod-lshift+ ,sdl2-ffi:+kmod-rshift+)
+    (:ctrl ,sdl2-ffi:+kmod-lctrl+ ,sdl2-ffi:+kmod-rctrl+)
+    (:meta ,sdl2-ffi:+kmod-lalt+)
+    (:super ,sdl2-ffi:+kmod-lgui+ ,sdl2-ffi:+kmod-rgui+)))
+
+(defun get-modifier (keysym)
+  (let* ((mod (sdl2:mod-value keysym))
+         (shift (mod-p mod :shift))
+         (ctrl (mod-p mod :ctrl))
+         (meta (mod-p mod :meta))
+         (super (mod-p mod :super)))
+    (make-modifier :shift shift :ctrl ctrl :meta meta :super super)))
